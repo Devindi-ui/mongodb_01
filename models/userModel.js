866b3c281665e3ffe195920540ee6af3bd6cac8d
Devindi-u <panchalidevindi@gmail.com>
2025-09-29 15:19:21 +0530

    initialize project

---
+++ models/userModel.js
@@ -0,0 +1,25 @@
+//User schema
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+        name:{
+            type: String,
+            required: true,
+            trim: true      
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true 
+        },
+
+        age:{
+            type: Number,
+            required: true 
+        }
+    }
+);
+
+module.exports = mongoose;
