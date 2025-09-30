0b29d45952aa55c504079446b21f8d8e644e2af7
Devindi-u <panchalidevindi@gmail.com>
2025-09-30 14:02:42 +0530

    get users

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,10 +6,35 @@ const userController = {
         try {
             const user = new User({name, email, age});
             await user.save();
+            return res.status(201).json({msg: 'User created successfully'});
+        } catch (error) {
+            if(error.code === 11000){
+                return res.status(400).json({msg: 'Email already exists'});
+            }
+            return res.status(500).json({msg: error.message});
+        }
+    },
+
+    getAllUser: async(req, res) => {
+        try {
+            const users = await User.find();
+            return res.status(200).json(users);
+        } catch (error) {
+            return res.status(500).json({msg: error.message});
+        }
+    },
+
+    getUserById: async(req, res) => {
+        const {id} = req.params;
+        try {
+            const user = await User.findById(id);
+            if(!user){
+                return res.status(404).json({msg: 'User not found'});
+            }
         } catch (error) {
             return res.status(500).json({msg: error.message});
         }
     }
-}
+};
 
 module.exports = userController;
--- routers/usersRoute.js
+++ routers/usersRoute.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.post('/create', userController.createUser);
+router.get('/all', userController.getAllUser);
+router.get('/find/:id', userController.getUserById);
 
 module.exports = router;
