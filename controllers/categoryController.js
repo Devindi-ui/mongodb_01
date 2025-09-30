1f2f19f3542556cfa9815f2d66d6930c05fdd84c
Devindi-u <panchalidevindi@gmail.com>
2025-09-30 22:51:09 +0530

    get category by id

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -19,6 +19,24 @@ const categoryController = {
         } catch (error) {
             return res.status(500),json({msg: error.message});
         }
+    },
+
+    getCategoryById: async(req, res) => {
+        try {
+            const {id} = req.params;
+            const category = await Category.findById(id);
+
+            if(!category){
+                return res.status(404).json({msg: 'Category not found.'});
+            }
+            return res.status(200).json(category);
+
+        } catch (error) {
+            if(error.kind === 'objectedId'){
+                return res.status(500).json({msg: error.message});
+            }
+            return res.status(500).json({msg: error.message});
+        }
     }
 };
 
--- routers/categoryRoute.js
+++ routers/categoryRoute.js
@@ -4,5 +4,6 @@ const categoryController = require('../controllers/categoryController');
 
 router.post('/create', categoryController.createCategory);
 router.get('/all', categoryController.getAllCategories);
+router.get('/find/:id', categoryController.getCategoryById);
 
 module.exports = router;
