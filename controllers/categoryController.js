d75a5f1a41dadd11e77c0706aa7577a0d8dff97a
Devindi-u <panchalidevindi@gmail.com>
2025-09-30 20:35:36 +0530

    get all categories

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -10,6 +10,16 @@ const categoryController = {
         } catch (error) {
             return res.status(500).json({msg: error.message});
         }
-}};
+    },
+
+    getAllCategories: async(req, res) => {
+        try {
+            const categories = await Category.find();
+            return res.status(200).json(categories);
+        } catch (error) {
+            return res.status(500),json({msg: error.message});
+        }
+    }
+};
 
 module.exports = categoryController;
--- routers/categoryRoute.js
+++ routers/categoryRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const categoryController = require('../controllers/categoryController');
 
 router.post('/create', categoryController.createCategory);
+router.get('/all', categoryController.getAllCategories);
 
 module.exports = router;
