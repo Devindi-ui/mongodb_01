7b6eb2ea1a779b07e2e65d140f2b348a462bdfb0
Devindi-u <panchalidevindi@gmail.com>
2025-09-30 19:16:06 +0530

    get all suppliers

--- controllers/supplierController.js
+++ controllers/supplierController.js
@@ -13,6 +13,15 @@ const SupplierController = {
             }
             return res.status(500).json({msg: error.message});
         }
+    },
+
+    getAllSuppliers: async(req, res) => {
+        try {
+            const suppliers = await Supplier.find();
+            return res.status(200).json(suppliers);
+        } catch (error) {
+            return res.status(500).json({msg: error.message});
+        }
     }
 };
 
--- routers/supplierRoute.js
+++ routers/supplierRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const supplierController = require('../controllers/supplierController');
 
 router.post('/create', supplierController.createSupplier);
+router.get('/all', supplierController.getAllSuppliers);
 
 module.exports = router;
